e3c8d67929fb10ccf35992f796f9bd6daa71454d
JacksonWengler <jwengler4304@stu.neisd.net>
2024-11-18 13:58:05 -0600

    g

--- createcode.py
+++ createcode.py
@@ -1,1 +1,12 @@
+import random
 
+a=[]
+num=0
+
+while (len(a)<100000):
+    num = random.randrange(00000,99999)
+    if num in a:
+        print("")
+    else:
+        print (num)
+        a.insert(num, num)
