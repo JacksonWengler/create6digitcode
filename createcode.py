3e38a8ea314936dd6914ec1a63b27d726bbea660
JacksonWengler <jwengler4304@stu.neisd.net>
2024-11-18 16:30:11 -0600

    added

--- createcode.py
+++ createcode.py
@@ -1,12 +1,18 @@
 import random
 
 a=[]
+f = open("passlist.txt", "a")
 num=0
 
-while (len(a)<100000):
+while (len(a)<99999):
     num = random.randrange(00000,99999)
     if num in a:
         print("")
     else:
         print (num)
         a.insert(num, num)
+        b=str(num)
+        c=(b)
+        d=str(c)
+        f.write(c)
+        f.write("\n")
